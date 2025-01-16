ae628cd8f2ad18e4f138361deec7f7fb8fcdc4aa
voyager-seb <seb@kolektivo.co>
2025-01-16 11:21:00 -0500

    feat(get chain info): logic modified

--- src/app/services/get-chain-info-service.ts
+++ src/app/services/get-chain-info-service.ts
@@ -11,6 +11,15 @@ const CONFIGS_URL = `${BASE_URL}/superchain/configs`
 const TOML_EXTENSION = '.toml'
 const SECURITY_COUNCIL_WALLET = '0x5a0Aae59D09fccBdDb6C6CcEB07B7279367C3d2A'
 
+
+
+const STANDARD_VALUE = 'Standard'
+const IMPLEMENTED_VALUE = 'Implemented'
+const NOT_IMPLEMENTED_VALUE = 'Not implemented'
+const NONE_VALUE = 'None'
+const UNKNOWN_VALUE = 'Unknown'
+const SECURITY_COUNCIL = 'Security Council'
+
 async function getTomlDataCached(url: string): Promise<string> {
   if (cache.has(url)) {
     return cache.get(url) ?? ''
@@ -39,9 +48,10 @@ export async function getChainInfoListService(): Promise<ChainInfo[]> {
 async function processChain(chain: any): Promise<ChainInfo> {
   const chainInfo = new ChainInfo()
   chainInfo.name = chain.name
-  chainInfo.layer = chain.parent?.type || 'Unknown'
-  chainInfo.status = getStatus(chain.identifier)
+  chainInfo.layer = chain.parent?.type || UNKNOWN_VALUE
+  chainInfo.status = chainInfo.type = getStatus(chain.identifier)
   chainInfo.configuration = getConfiguration(chain.superchain_level)
+  chainInfo.scStatus = chain.superchain_level
 
   const detailUrl = `${CONFIGS_URL}/${chain.identifier}${TOML_EXTENSION}`
   return await setChainInfoDetail(detailUrl, chainInfo)
@@ -52,12 +62,12 @@ async function setChainInfoDetail(url: string, chainInfo: ChainInfo): Promise<Ch
     const response = await getTomlDataCached(url)
     const detailData = toml.parse(response)
 
+    chainInfo.charter = detailData.standard_chain_candidate ? STANDARD_VALUE : NONE_VALUE
     chainInfo.upgradeKeys = getUpgradeKeys(detailData.addresses)
     chainInfo.faultProofs = getFaultProofs(detailData.addresses)
-    chainInfo.decentStage = getDecentStage(chainInfo)
-    chainInfo.charter = 'N/A'
+    chainInfo.stage = chainInfo.decentStage = getDecentStage(chainInfo)
     chainInfo.charterLink = ''
-    chainInfo.dataAvail = detailData.data_availability_type?.toUpperCase() || 'Unknown'
+    chainInfo.dataAvail = detailData.data_availability_type?.toUpperCase() || UNKNOWN_VALUE
     chainInfo.dataAvailLink = ''
     chainInfo.blockTime = `~${detailData.block_time}`
 
@@ -69,17 +79,17 @@ async function setChainInfoDetail(url: string, chainInfo: ChainInfo): Promise<Ch
 }
 
 function getDecentStage(chainInfo: ChainInfo): string {
-  if (chainInfo.upgradeKeys === 'Security Council' && chainInfo.faultProofs === 'Implemented') return 'Stage 1'
+  if (chainInfo.upgradeKeys === SECURITY_COUNCIL && chainInfo.faultProofs === IMPLEMENTED_VALUE) return 'Stage 1'
   else return 'Stage 0'
 }
 
 function getFaultProofs(addresses: Record<string, any>): string {
-  if (addresses.FaultDisputeGame) return 'Implemented'
-  else return 'Not implemented'
+  if (addresses.FaultDisputeGame) return IMPLEMENTED_VALUE
+  else return NOT_IMPLEMENTED_VALUE
 }
 
 function getUpgradeKeys(addresses: Record<string, any>): string {
-  if (addresses.ProxyAdminOwner?.toUpperCase() === SECURITY_COUNCIL_WALLET.toUpperCase()) return 'Security Council'
+  if (addresses.ProxyAdminOwner?.toUpperCase() === SECURITY_COUNCIL_WALLET.toUpperCase()) return SECURITY_COUNCIL
   else return 'Unspecified'
 }
 
@@ -89,6 +99,6 @@ function getStatus(identifier: string): string {
 }
 
 function getConfiguration(superchainLevel: number): string {
-  if (superchainLevel === 1) return 'Standard'
+  if (superchainLevel === 1) return STANDARD_VALUE
   else return 'Frontier'
 }
--- src/app/types/chain-info.ts
+++ src/app/types/chain-info.ts
@@ -2,13 +2,17 @@ export class ChainInfo {
   name!: String
   layer!: String
   status!: String
+  type!: String
   configuration!: String
   upgradeKeys!: String
   faultProofs!: String
   decentStage!: String
+  stage!: String
   charter!: String
   charterLink!: String
   dataAvail!: String
   dataAvailLink!: String
   blockTime!: String
+  scStatus!:String
+  gasToken!:String
 }
